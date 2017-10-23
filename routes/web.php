c41712fff0160facd9e131f27a547b267285c531
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-10-23 15:47:28 +0800

    練習四: 正規表達式限制參數

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,17 @@ Route::get('student/{student_no}/score/{subject}', function ($student_no,$subjec
     return '學號：'.$student_no.'的'.$subject.'成績';
 });
 */
-
+/*
 //練習三: 提供學生查詢所有科目或特定科目的成績
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
 });
+*/
+
+//練習四: 正規表達式限制參數
+Route::get('student/{student_no}',function ($student_no){
+    return '學號：'.$student_no;
+}) -> where(['student_no' => 's[0-9]{10}']);
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
+    return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
+}) -> where(['student_no' => 's[0-9]{10}','subject' => '(chinese|english|math)']);
