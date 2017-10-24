ecc8700479ef74b1909401d83a1ee97446afa2e1
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-10-24 16:44:06 +0800

    Ch07(練習一)-4 : 建立主樣板master.blade.php的內容

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,15 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2017/10/24
- * Time: 下午 04:34
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+   @section('head')
+       @include('partials.head')
+    @show
+</head>
+<body>
+    @include('partials.nav')
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
