b8fb96b929f26dbc22136fda4c3abecd97d3fbf8
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-10-24 23:51:41 +0800

    ch07(練習三)-3 : 修改StudentController的 getStudentData 和 getStudentScore 兩個方法，使之可傳入student_no或subject的資料

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,14 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use View;
 
 class StudentController extends Controller
 {
-    //練習四: 加入getStudentData及getStudentScore函數
+
     public function getStudentData($student_no){
-        return "學號: ".$student_no;
+        //練習四: 加入getStudentData及getStudentScore函數
+        // return "學號: ".$student_no;
+
+        //ch07(練習三)-3修改StudentController的 getStudentData 方法，使之可傳入student_no或subject的資料
+        return View::make('score', [
+            'student_no' => $student_no,
+            'subject' => null
+        ]);
     }
     public function getStudentScore($student_no, $subject = null){
-        return "學號：" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "成績";
+       // return "學號：" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "成績";
+
+        //ch07(練習三)-3修改StudentController的 getStudentScore 方法，使之可傳入student_no或subject的資料
+        return View::make('score', [
+            'student_no' => $student_no,
+            'subject' => $subject
+        ]);
     }
 }
