cee29569532cad59733c8dd89edd92e2785f0697
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-11-21 01:43:34 +0800

    ch08(練習三)-2 : 建立student 及 score 兩者間的一對一關係

--- app/Score.php
+++ app/Score.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use \App\Student as StudentEloquent;
 class Score extends Model
 {
-    //
+    protected $table = 'score';
+
+    //成績屬於一位學生的 (a Score belongTo a Student)
+    public function student(){
+        return $this->belongsTo(StudentEloquent::class);
+    }
 }
--- app/Student.php
+++ app/Student.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use \App\Score as ScoreEloquent;
 class Student extends Model
 {
-    //
+    protected $table = 'student';
+
+    //學生對應到一個成績 (a Student hasOne Score)
+    public function score(){
+        return $this->hasOne(ScoreEloquent::class);
+    }
 }
