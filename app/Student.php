8f9b9ce63f2694c51c7ef4aa2612458ccb53836e
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-11-21 01:51:16 +0800

    ch08(練習三)-3 : 建立Student 及 User 兩者間的一對一關係

--- app/Student.php
+++ app/Student.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use \App\User as UserEloquent;
 use \App\Score as ScoreEloquent;
 class Student extends Model
 {
@@ -12,5 +12,10 @@ class Student extends Model
     //學生對應到一個成績 (a Student hasOne Score)
     public function score(){
         return $this->hasOne(ScoreEloquent::class);
+    }
+
+    //一個學生有一個使用者帳號 (a Student belongTo a User)
+    public function user(){
+        return $this->belongsTo(UserEloquent::class);
     }
 }
