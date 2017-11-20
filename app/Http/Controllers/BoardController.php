d14d7dc95e210fd384fb12467f40758ef199f5f5
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-11-21 02:09:37 +0800

    ch08(練習四)-2:修改 BoardController 之 getIndex()方法

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -12,6 +12,14 @@ use View;
 class BoardController extends Controller
 {
     public function getIndex(){
-        return view('board');
+        //return view('board');
+
+        //課本寫法:
+        //return View::make('board',['scores'=>Score::with('student')->OrderByTotal()->orderBySubject()->get()]);
+
+        //由 DB 取得學生的國英數成績資料($scores)，且依成績高低排列(依總分排列，再依國英數)後，傳遞給 board 視圖呈現之
+        $scores = Score::orderByTotal()->orderBySubject()->get();
+        $data = ['scores'=>$scores];
+        return view('board',$data);
     }
 }
