fb213ad78ac621661dbcd498e990ecf580e6a267
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-11-21 17:09:13 +0800

    ch08(練習六):撰寫seeder->建立一個使用者、學生及分數資料

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User as UserEloquent;
+use App\Student as StudentEloquent;
+use App\Score as ScoreEloquent;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $user=UserEloquent::create([
+           'name'=>'小明',
+           'email'=>'min@mail.com',
+           'password'=>bctypt('abc123')
+        ]);
+
+        $student=StudentEloquent::create([
+           'user_id'=>$user->id,
+           'no'=>'s1234567890',
+           'tel'=>'0912345678'
+        ]);
+
+        $score=ScoreEloquent::create([
+           'student_id'=>$student->id,
+           'chinese'=>60,
+           'english'=>60,
+           'math'=>60,
+           'total'=>180
+        ]);
+
     }
 }
